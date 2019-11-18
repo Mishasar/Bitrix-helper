819973c648b586bb41a2f35b82a4f7a5dbc4fecc
Mikhail Saraykin <saraykin1996@gmail.com>
2019-11-18 09:45:25 +0700

    add event handler for bonus system

--- php_interface/init.php
+++ php_interface/init.php
@@ -7,30 +7,7 @@ require $_SERVER["DOCUMENT_ROOT"] . '/local/vendor/autoload.php';
 \Bitrix\Main\Loader::includeModule('letsrock.lib');
 \Bitrix\Main\Loader::includeModule('sale');
 
-
-
 \Bitrix\Main\EventManager::getInstance()->addEventHandler(
     'sale',
     'OnSaleStatusOrderChange',
     ['Letsrock\\Lib\\Controllers\\OrderController', 'orderBonusHandler']);
-
-class Handler
-{
-    function OnSaleStatusOrderChange($event)
-    {
-        $parameters = $event->getParameters();
-        if ($parameters['VALUE'] === 'F') {
-            /** @var \Bitrix\Sale\Order $order */
-            $order = $parameters['ENTITY'];
-            /**
-             *
-             * Your code is here
-             *
-             */
-        }
-
-        return new \Bitrix\Main\EventResult(
-            \Bitrix\Main\EventResult::SUCCESS
-        );
-    }
-}
