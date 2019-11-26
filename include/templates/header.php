5443d59691c6141ab7cff6811700987f89c46f5c
Mikhail Saraykin <saraykin1996@gmail.com>
2019-11-26 10:56:54 +0700

    little fixes

--- include/templates/header.php
+++ include/templates/header.php
@@ -6,7 +6,6 @@
     <?
 
     use Bitrix\Main\Page\Asset;
-    use Letsrock\Lib\Models\Property;
 
     CUtil::InitJSCore();
     CJSCore::Init(['ajax', 'json', 'ls', 'session', 'jquery', 'popup', 'pull']);
@@ -71,50 +70,52 @@
                                 ],
                                 false
                             ); ?>
-                            <? $APPLICATION->IncludeComponent("bitrix:sale.basket.basket.line", "header", [
-                                "HIDE_ON_BASKET_PAGES" => "Y",
-                                // Не показывать на страницах корзины и оформления заказа
-                                "PATH_TO_BASKET" => SITE_DIR . "cart/",
-                                // Страница корзины
-                                "PATH_TO_ORDER" => SITE_DIR . "personal/order/make/",
-                                // Страница оформления заказа
-                                "PATH_TO_PERSONAL" => SITE_DIR . "personal/",
-                                // Страница персонального раздела
-                                "PATH_TO_PROFILE" => SITE_DIR . "personal/",
-                                // Страница профиля
-                                "PATH_TO_REGISTER" => SITE_DIR . "login/",
-                                // Страница регистрации
-                                "POSITION_FIXED" => "Y",
-                                // Отображать корзину поверх шаблона
-                                "POSITION_HORIZONTAL" => "right",
-                                // Положение по горизонтали
-                                "POSITION_VERTICAL" => "top",
-                                // Положение по вертикали
-                                "SHOW_AUTHOR" => "Y",
-                                // Добавить возможность авторизации
-                                "SHOW_DELAY" => "N",
-                                // Показывать отложенные товары
-                                "SHOW_EMPTY_VALUES" => "Y",
-                                // Выводить нулевые значения в пустой корзине
-                                "SHOW_IMAGE" => "Y",
-                                // Выводить картинку товара
-                                "SHOW_NOTAVAIL" => "N",
-                                // Показывать товары, недоступные для покупки
-                                "SHOW_NUM_PRODUCTS" => "Y",
-                                // Показывать количество товаров
-                                "SHOW_PERSONAL_LINK" => "N",
-                                // Отображать персональный раздел
-                                "SHOW_PRICE" => "Y",
-                                // Выводить цену товара
-                                "SHOW_PRODUCTS" => "Y",
-                                // Показывать список товаров
-                                "SHOW_SUMMARY" => "Y",
-                                // Выводить подытог по строке
-                                "SHOW_TOTAL_PRICE" => "Y",
-                                // Показывать общую сумму по товарам
-                            ],
-                                false
-                            ); ?>
+                            <? if ($USER->IsAuthorized()) {
+                                $APPLICATION->IncludeComponent("bitrix:sale.basket.basket.line", "header", [
+                                    "HIDE_ON_BASKET_PAGES" => "Y",
+                                    // Не показывать на страницах корзины и оформления заказа
+                                    "PATH_TO_BASKET" => SITE_DIR . "cart/",
+                                    // Страница корзины
+                                    "PATH_TO_ORDER" => SITE_DIR . "personal/order/make/",
+                                    // Страница оформления заказа
+                                    "PATH_TO_PERSONAL" => SITE_DIR . "personal/",
+                                    // Страница персонального раздела
+                                    "PATH_TO_PROFILE" => SITE_DIR . "personal/",
+                                    // Страница профиля
+                                    "PATH_TO_REGISTER" => SITE_DIR . "login/",
+                                    // Страница регистрации
+                                    "POSITION_FIXED" => "Y",
+                                    // Отображать корзину поверх шаблона
+                                    "POSITION_HORIZONTAL" => "right",
+                                    // Положение по горизонтали
+                                    "POSITION_VERTICAL" => "top",
+                                    // Положение по вертикали
+                                    "SHOW_AUTHOR" => "Y",
+                                    // Добавить возможность авторизации
+                                    "SHOW_DELAY" => "N",
+                                    // Показывать отложенные товары
+                                    "SHOW_EMPTY_VALUES" => "Y",
+                                    // Выводить нулевые значения в пустой корзине
+                                    "SHOW_IMAGE" => "Y",
+                                    // Выводить картинку товара
+                                    "SHOW_NOTAVAIL" => "N",
+                                    // Показывать товары, недоступные для покупки
+                                    "SHOW_NUM_PRODUCTS" => "Y",
+                                    // Показывать количество товаров
+                                    "SHOW_PERSONAL_LINK" => "N",
+                                    // Отображать персональный раздел
+                                    "SHOW_PRICE" => "Y",
+                                    // Выводить цену товара
+                                    "SHOW_PRODUCTS" => "Y",
+                                    // Показывать список товаров
+                                    "SHOW_SUMMARY" => "Y",
+                                    // Выводить подытог по строке
+                                    "SHOW_TOTAL_PRICE" => "Y",
+                                    // Показывать общую сумму по товарам
+                                ],
+                                    false
+                                );
+                            } ?>
                         </div>
                     </div>
                 </div>
